fd1b93c231ff05ece394bf0393a70090db03352f
Jason <jasonsoloriom@gmail.com>
2018-02-16 11:14:57 -0700

    Importing the QuotesPage, so that we can then use it to push the page when a category is clicked

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Quote } from "../../data/quotes.interface"
-
 import quotes from "../../data/quotes"
+import { QuotesPage } from "../quotes/quotes"
+
 @Component({
   selector: 'page-library',
   templateUrl: 'library.html',
@@ -10,6 +11,7 @@ import quotes from "../../data/quotes"
 export class LibraryPage implements OnInit{
 
   quoteCollection: {category: string, quotes: Quote[], icon: string}[]
+  quotesPage = QuotesPage
 
   ngOnInit(){
     this.quoteCollection = quotes;
