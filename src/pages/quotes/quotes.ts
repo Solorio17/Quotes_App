367095c6a9cee77f1cc5c9d1da53e46d3fbd7bdb
Jason <jasonsoloriom@gmail.com>
2018-02-20 14:18:19 -0700

    Using AlertController to alert a user when they favorite a quote

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { NavParams } from "ionic-angular"
+import { NavParams, AlertController } from "ionic-angular"
 
 import { Quote } from "../../data/quotes.interface"
 
@@ -14,9 +14,32 @@ export class QuotesPage implements OnInit {
 
 quoteGroup: {category: string, quotes: Quote[], icon: string}
 
- constructor(private navParams : NavParams){}
+ constructor(private navParams : NavParams, private alertCrtl : AlertController){}
 
  ngOnInit(){
    this.quoteGroup = this.navParams.data;
+ }
+
+ onAddToFavorite(selectedQuote : Quote){
+   const alert = this.alertCrtl.create({
+     title: "Add Quote",
+     message: "Are you sure you want to favorite this quote?",
+     buttons: [
+      {
+       text: "Yes, go ahead",
+       handler: () => {
+         console.log('Ok')}
+      },
+      {
+        text: "No, I change my mind",
+        role: 'cancel',
+        handler: () => {
+          console.log("Cancelled")
+        }
+      }
+   ]
+   });
+
+   alert.present();
  }
 }
